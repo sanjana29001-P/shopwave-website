105bd1f6262131a663c92793342d01b0c0e05872
Spoorthi P N <spoorthipn17@gmail.com>
2026-06-11 20:38:27 +0530

    Added ShopWave UI improvements and images

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 function addToCart(product){
 
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let cart =
+JSON.parse(localStorage.getItem("cart")) || [];
 
 cart.push(product);
 
-localStorage.setItem("cart", JSON.stringify(cart));
+localStorage.setItem(
+"cart",
+JSON.stringify(cart)
+);
 
-alert(product + " added to cart!");
+alert(product + " added to cart");
 }
 
 if(document.getElementById("cartItems")){
 
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let cart =
+JSON.parse(localStorage.getItem("cart")) || [];
 
-let list = document.getElementById("cartItems");
+let list =
+document.getElementById("cartItems");
 
-cart.forEach(item => {
+cart.forEach(item=>{
 
-let li = document.createElement("li");
+let li =
+document.createElement("li");
 
-li.textContent = item;
+li.innerText=item;
 
 list.appendChild(li);
 
 });
+
 }
